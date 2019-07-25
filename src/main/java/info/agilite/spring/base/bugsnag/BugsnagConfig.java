429b350f2814ed93eef0d6e3e649215b1049b437
tatiane <tatiane@multitecsistemas.com.br>
2019-07-25 09:32:47 -0300

    bugsnag

--- src/main/java/info/agilite/spring/base/bugsnag/BugsnagConfig.java
+++ src/main/java/info/agilite/spring/base/bugsnag/BugsnagConfig.java
@@ -1,5 +1,6 @@
 package info.agilite.spring.base.bugsnag;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -7,11 +8,20 @@ import org.springframework.context.annotation.Import;
 import com.bugsnag.Bugsnag;
 import com.bugsnag.BugsnagSpringConfiguration;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
 @Configuration
 @Import(BugsnagSpringConfiguration.class)
+@FieldDefaults(makeFinal=true, level=AccessLevel.PRIVATE)
+@AllArgsConstructor(access=AccessLevel.PACKAGE)
 public class BugsnagConfig {
+	@Value("${info.agilite.spring.base.crash-report-key:#{null}}")
+	String bugsnagKey;
+	
 	@Bean
 	public Bugsnag bugsnag() {
-		return new Bugsnag("");
+		return new Bugsnag(bugsnagKey);
 	}
 }
